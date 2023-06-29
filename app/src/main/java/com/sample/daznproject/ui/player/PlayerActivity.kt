06e9758a1604a7a3bca1083150e57f571c5c3a56
Snehitha2112 <snehithachinnapally.21@gmail.com>
2023-06-29 07:24:53 +0530

    PlayerActivity chnages

--- app/src/main/java/com/sample/daznproject/ui/player/PlayerActivity.kt
+++ app/src/main/java/com/sample/daznproject/ui/player/PlayerActivity.kt
@@ -9,15 +9,15 @@ import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
 import androidx.media3.common.C
 import androidx.media3.common.MediaItem
-import androidx.media3.common.util.Util
 import androidx.media3.datasource.DataSource
 import androidx.media3.datasource.DefaultDataSource
 import androidx.media3.exoplayer.ExoPlayer
@@ -28,9 +28,6 @@ import com.sample.daznproject.ui.theme.DaznProjectTheme
 
 class PlayerActivity : ComponentActivity() {
 
-    private var exoPlayer: ExoPlayer? = null
-    var playbackPosition: Long = 0
-    var currentWindow = 0
     private var pauseCount = 0
     private var forwardCount = 0
     private var backwardCount = 0
@@ -43,95 +40,79 @@ class PlayerActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    VideoPlayerView(
-                        intent.getStringExtra(MainActivity.SELECTED_VIDEO_URL)
-                        /*"asset:///food.mp4"*/
-                    )
-                }
-            }
-        }
-    }
-
-    override fun onStart() {
-        super.onStart()
-        restartPlayer()
-    }
-
-    override fun onPause() {
-        super.onPause()
-        if (Util.SDK_INT <= 23) {
-            stopPlayer()
-        }
-    }
-
-    override fun onStop() {
-        super.onStop()
-        if (Util.SDK_INT > 23) {
-            stopPlayer()
-        }
-    }
-
-    private fun stopPlayer() {
-        if (exoPlayer != null) {
-            playbackPosition = exoPlayer!!.currentPosition
-            currentWindow = exoPlayer!!.currentMediaItemIndex
-            exoPlayer!!.playWhenReady = false
-        }
-    }
-
-    private fun restartPlayer() {
-        exoPlayer?.seekTo(currentWindow, playbackPosition)
-    }
-
-    @Composable
-    fun VideoPlayerView(videoUrl: String?) {
-        val padding = 8.dp
-        Column(
-            modifier = Modifier
-                .padding(padding)
-                .fillMaxWidth()
-        ) {
-            Card(
-                elevation = 8.dp,
-                modifier = Modifier.fillMaxWidth()
-            ) {
-                videoUrl?.let { uri ->
-                    exoPlayer = remember {
-                        buildExoPlayer(this@PlayerActivity, uri = uri)
+                    val videoUrl = intent.getStringExtra(MainActivity.SELECTED_VIDEO_URL)
+                    val padding = 8.dp
+                    var lifecycle by remember {
+                        mutableStateOf(Lifecycle.Event.ON_CREATE)
                     }
-                    exoPlayer?.playWhenReady = true
-                    exoPlayer?.videoScalingMode = C.VIDEO_SCALING_MODE_SCALE_TO_FIT_WITH_CROPPING
+                    val lifeCycleOwner = LocalLifecycleOwner.current
+                    DisposableEffect(lifeCycleOwner) {
+                        val observer = LifecycleEventObserver { _, event ->
+                            lifecycle = event
+                        }
+                        lifeCycleOwner.lifecycle.addObserver(observer)
+                        onDispose {
+                            lifeCycleOwner.lifecycle.removeObserver(observer)
+                        }
+                    }
 
-                    DisposableEffect(
-                        AndroidView(
-                            factory = {
-                                PlayerView(it).apply {
-                                    player = exoPlayer
-                                }
-                            }, modifier = Modifier
-                                .height(height = 200.dp)
-                                .fillMaxWidth()
-                        )
+                    Column(
+                        modifier = Modifier
+                            .padding(padding)
+                            .fillMaxWidth()
                     ) {
-                        onDispose { exoPlayer?.release() }
+                        Card(
+                            elevation = 8.dp,
+                            modifier = Modifier.fillMaxWidth()
+                        ) {
+                            videoUrl?.let { uri ->
+                                val exoPlayer = buildExoPlayer(this@PlayerActivity, uri = uri)
+                                exoPlayer.playWhenReady = true
+                                exoPlayer.videoScalingMode =
+                                    C.VIDEO_SCALING_MODE_SCALE_TO_FIT_WITH_CROPPING
+
+                                AndroidView(
+                                    factory = { context ->
+                                        PlayerView(context).also {
+                                            it.player = exoPlayer
+                                        }
+                                    },
+                                    update = {
+                                        when (lifecycle) {
+                                            Lifecycle.Event.ON_PAUSE -> {
+                                                it.onResume()
+                                                it.player?.pause()
+                                            }
+                                            Lifecycle.Event.ON_RESUME -> {
+                                                it.onResume()
+                                            }
+                                            else -> Unit
+                                        }
+                                    },
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .aspectRatio(16 / 9f)
+                                )
+                            }
+                        }
+
+                        Text(
+                            text = "Pause Count :: $pauseCount",
+                            modifier = Modifier.padding(15.dp)
+                        )
+                        Text(
+                            text = "Forward Count :: $pauseCount",
+                            modifier = Modifier.padding(horizontal = 15.dp)
+                        )
+                        Spacer(modifier = Modifier.size(15.dp))
+                        Text(
+                            text = "Backward Count :: $pauseCount",
+                            modifier = Modifier.padding(horizontal = 15.dp)
+                        )
+
                     }
                 }
             }
-
-            Text(
-                text = "Pause Count :: $pauseCount",
-                modifier = Modifier.padding(15.dp)
-            )
-            Text(
-                text = "Forward Count :: $pauseCount",
-                modifier = Modifier.padding(horizontal = 15.dp)
-            )
-            Spacer(modifier = Modifier.size(15.dp))
-            Text(
-                text = "Backward Count :: $pauseCount",
-                modifier = Modifier.padding(horizontal = 15.dp)
-            )
-
         }
     }
 
